78b262f5bed770e367b3001986264b76102c6723
Akshita Sharma <akshitasharma8088@gmail.com>
2026-07-31 03:34:39 +0000

    comparison

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,11 @@
 let score="33"
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
-let valueInNumber=Number(score);
-console.log(typeof valueInNumber); 
-console.log(valueInNumber);
+// let valueInNumber=Number(score);
+// console.log(typeof valueInNumber); 
+// console.log(valueInNumber);
 
 //"33"=>33
 //"33abc"=>NaN
@@ -25,3 +25,46 @@ let someNumber=33
 // let stringNumber=String(someNumber)
 // console.log(stringNumber)
 // console.log(typeof stringNumber)
+
+//*************************** Operations ***************
+let value=3
+let negValue= -value
+// console.log(negValue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/3);
+// console.log(2**3);
+// console.log(2%3);
+
+let str1="hello"
+let str2=" Akshita"
+
+let str3=str1+str2
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+"2"+2);
+// console.log(1+2+"2");
+
+// console.log((3+4)*5%3);
+
+// console.log(+true);
+// console.log(+"")
+
+let num1,num2,num3
+num1=num2=num3=2+2
+
+// let gameCounter=100
+// ++gameCounter;
+// console.log(gameCounter)
+
+let x=3;
+const y=++x;
+console.log(x,y);
+
+let a=1;
+const b=a++;
+console.log(a,b);
