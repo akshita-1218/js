e05817bec471a7606a7d568376994574c094931c
Akshita Sharma <akshitasharma8088@gmail.com>
2026-08-15 16:57:27 +0000

    BASIC LOOPS

---
+++ 05_iterations/one.js
@@ -0,0 +1,33 @@
+// for
+// for(let i=1;i<=10;i++){
+//     if(i==5){
+//         console.log("5 is the best number")
+//     }
+//     // console.log(i);
+// }
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`Outer loop value: ${i}`);
+//     for (let j = 1; j <= 10; j++) {
+//         // console.log(`Inner loop value: ${j}`);
+//         console.log(i+'*'+j+'='+i*j);
+//     }    
+// }
+
+// let myArray=["flash","batman","superman"]
+// for (let index = 0; index < myArray.length; index++) {
+//     const element = myArray[index];
+//     // console.log(element);
+//     console.log(myArray.length)
+// }
+
+// break and continue
+for (let index = 0; index <= 8; index++) {
+    if(index==5){
+        // console.log("got 5 now bye")
+        // break;
+        // continue;
+    }
+    console.log(`Value of i is ${index}`)
+    
+}
