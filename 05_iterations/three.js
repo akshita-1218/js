7e3d7bcfcb99db4ad52be1b1fc5fbad72b4da87b
Akshita Sharma <akshitasharma8088@gmail.com>
2026-08-16 04:37:14 +0000

    for of arrays

---
+++ 05_iterations/three.js
@@ -0,0 +1,32 @@
+// // for of
+
+// const arr=[1,2,3,4,5]
+// for (const i of arr){
+//     console.log(i);
+// }
+
+// const greetings="Hello World !"
+// for(const greet of greetings){
+//     console.log(`Each char is : ${greet}`)
+// }
+
+//MAPS
+const map=new Map()
+map.set('IN',"India")
+map.set('UK',"United Kingdom")
+map.set('FR',"France")
+
+console.log(map);
+
+for(const key of map){
+    console.log(key)
+}
+
+for(const [key, value] of map){
+    console.log(key, ':-', value)
+}
+
+const myObject = {
+    'game1' : 'NFS',
+    'game2' : 'spiderman'
+}
